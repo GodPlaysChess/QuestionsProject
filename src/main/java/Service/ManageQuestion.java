74189ab5e5a508c3925fb665c6e218e8733700b5
Gleb <pumabox@gmail.com>
2013-07-07 22:20:17 +0200

    Abstraction and implementation is separated

--- src/main/java/Service/ManageQuestion.java
+++ src/main/java/Service/ManageQuestion.java
@@ -1,92 +1,13 @@
 package Service;
 
-import org.hibernate.HibernateException;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.classic.Session;
+public interface ManageQuestion {
 
-import java.util.Iterator;
-import java.util.List;
+    public void deleteQuestion(Integer QuestionID);
 
-public class ManageQuestion {
-    private static SessionFactory factory;
+    public void updateQuestion(Integer QuestionID, String newText);
 
-    public Integer addQuestion(String type, String text) {
-        Session session = factory.openSession();
-        Transaction tx = null;
-        Integer QuestionID = null;
-        try {
-            tx = session.beginTransaction();
-            Question Question = new Question(type, text);
-            QuestionID = (Integer) session.save(Question);
-            tx.commit();
-        } catch (HibernateException e) {
-            if (tx != null) tx.rollback();
-            e.printStackTrace();
-        } finally {
-            session.close();
-        }
-        return QuestionID;
-    }
+    public void listQuestions();
 
-    /* Method to READ all questions */
-    public void listQuestions() {
-        Session session = factory.openSession();
-        Transaction tx = null;
-        try {
-            tx = session.beginTransaction();
-            List Questions = session.createQuery("FROM Question").list();
-            for (Iterator iterator =
-                         Questions.iterator(); iterator.hasNext(); ) {
-                Question Question = (Question) iterator.next();
-                System.out.print("Type: |" + Question.getType());
-                System.out.print("  Text: " + Question.getText());
-            }
-            tx.commit();
-        } catch (HibernateException e) {
-            if (tx != null) tx.rollback();
-            e.printStackTrace();
-        } finally {
-            session.close();
-        }
-    }
-
-    /* Method to UPDATE text of the question */
-    public void updateQuestion(Integer QuestionID, String newText) {
-        Session session = factory.openSession();
-        Transaction tx = null;
-        try {
-            tx = session.beginTransaction();
-            Question Question =
-                    (Question) session.get(Question.class, QuestionID);
-            Question.setText(newText);
-            session.update(Question);
-            tx.commit();
-        } catch (HibernateException e) {
-            if (tx != null) tx.rollback();
-            e.printStackTrace();
-        } finally {
-            session.close();
-        }
-    }
-
-    /* Method to DELETE the Question from the records */
-    public void deleteQuestion(Integer QuestionID) {
-        Session session = factory.openSession();
-        Transaction tx = null;
-        try {
-            tx = session.beginTransaction();
-            Question Question =
-                    (Question) session.get(Question.class, QuestionID);
-            session.delete(Question);
-            tx.commit();
-        } catch (HibernateException e) {
-            if (tx != null) tx.rollback();
-            e.printStackTrace();
-        } finally {
-            session.close();
-        }
-    }
+    public Integer addQuestion(String type, String text);
 
 }
