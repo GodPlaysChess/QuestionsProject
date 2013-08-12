a893d3cf22ceef3f8f7b54019e4a7e86e2ae7917
Gleb <pumabox@gmail.com>
2013-08-12 15:35:58 +0200

    Updated TestCourseDAO

--- src/test/java/examination/DataLayer/dao/TestCourseDao.java
+++ src/test/java/examination/DataLayer/dao/TestCourseDao.java
@@ -1,6 +1,9 @@
 package examination.DataLayer.dao;
 
+import examination.DataLayer.models.Course;
 import examination.DataLayer.models.Exam;
+import examination.DataLayer.models.GradingSystem;
+import examination.DataLayer.models.Question;
 import org.apache.log4j.Logger;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,7 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
@@ -19,20 +24,19 @@ import static org.junit.Assert.assertTrue;
 public class TestCourseDao {
 
     @Autowired
-    private ExamDAO examDAO;
+    private CourseDAO courseDAO;
     private static final Logger log = Logger.getLogger(TestExaminationDao.class);
 
     @Test
     public void test() {
-        /* Insert Examination */
-        Exam exam = new Exam();
-        exam.setCourseId(1);
-        exam.setStudentId(2);
-        exam.setTimeStart(new Date());
-        exam.setTimeFinish(new Date());
-        exam.setCurrentQuestion(3);
-        //exam.setQuestionIds()
-        boolean inserted = examDAO.insert(exam);
+        /* Insert Course */
+        Course course = new Course();
+        GradingSystem gradingSystem = new GradingSystem();
+        course.setGradingSystem(gradingSystem);
+        course.setExamQuestionsNumber(5);
+        List<Question> questions = new ArrayList<Question>(1);
+
+        boolean inserted = courseDAO.insert(course);
         assertTrue(inserted);
 
         long id = exam.getId();
