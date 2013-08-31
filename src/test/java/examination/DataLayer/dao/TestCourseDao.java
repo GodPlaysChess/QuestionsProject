c5985b1d079ad8a42b76b6ead33e475826803718
Alex Sav <asavanster@gmail.com>
2013-09-01 00:46:49 +0400

    test refactor

--- src/test/java/examination/DataLayer/dao/TestCourseDao.java
+++ src/test/java/examination/DataLayer/dao/TestCourseDao.java
@@ -1,33 +1,20 @@
 package examination.DataLayer.dao;
 
 import examination.DataLayer.models.Course;
-import examination.DataLayer.models.Exam;
 import examination.DataLayer.models.GradingSystem;
 import examination.DataLayer.models.Question;
-import org.apache.log4j.Logger;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
-
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath*:/spring/applicationContext.xml"})
-public class TestCourseDao {
+public class TestCourseDao  extends AbstractTest<Course> {
 
     @Autowired
     private CourseDAO courseDAO;
     @Autowired
     private QuestionDAO questionDAO;
-    private static final Logger log = Logger.getLogger(TestExaminationDao.class);
 
     @Test
     public void test() {
@@ -40,18 +27,6 @@ public class TestCourseDao {
         questions.add(questionDAO.selectById(1));  // Change to getRandomQuestion()
         questions.add(questionDAO.selectById(2));  // when this method is finished
 
-        boolean inserted = courseDAO.insert(course);
-        assertTrue(course.getId() > 0);
-        assertTrue(inserted);
-
-        long id = course.getId();
-
-        /* GET course */
-        course = courseDAO.selectById(id);
-        assertNotNull(course);
-
-        /* DELETE course */
-        courseDAO.delete(id);
-        assertNull(courseDAO.selectById(id));
+        baseCheck(course, courseDAO);
     }
 }
--- src/test/java/examination/DataLayer/dao/TestQuestionManager.java
+++ src/test/java/examination/DataLayer/dao/TestQuestionManager.java
@@ -3,26 +3,18 @@ package examination.DataLayer.dao;
 
 import examination.DataLayer.models.Question;
 import examination.DataLayer.models.enums.QuestionType;
-import org.apache.log4j.Logger;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath*:/spring/applicationContext.xml"})
-public class TestQuestionManager {
+public class TestQuestionManager extends AbstractTest<Question> {
 
     @Autowired
     private QuestionDAO questionDAO;
-    private static final Logger log = Logger.getLogger(TestQuestionManager.class);
-
 
     @Test
     public void test() {
@@ -31,18 +23,8 @@ public class TestQuestionManager {
         Question question = new Question();
         question.setText("this message must be deleted");
         question.setType(QuestionType.RADIOBUTTON);
-        boolean result = questionDAO.insert(question);
-        assertTrue(question.getId() > 0);
-        assertTrue(result);
 
-        /* GET Question */
-        Question quest = questionDAO.selectById(question.getId());
-        assertNotNull(quest);
-
-        /* DELETE Question */
-        questionDAO.delete(quest.getId());
-        Question questDeleted = questionDAO.selectById(question.getId());
-        assertNull(questDeleted);
+        baseCheck(question, questionDAO);
 
         /* GET LIST with OFFSET Question */
         List<Long> ids = new ArrayList<Long>();
@@ -69,8 +51,6 @@ public class TestQuestionManager {
         for (long id : ids){
             questionDAO.delete(id);
         }
-
-        log.info(questionDAO);
     }
 
 }
