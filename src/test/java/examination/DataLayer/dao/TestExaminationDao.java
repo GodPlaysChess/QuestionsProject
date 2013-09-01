2c805f19cd44c6fa0e27207c2de4e7c5a8b88d3f
GodPlaysChess <pumabox@gmail.com>
2013-09-01 17:57:05 +0200

    test

--- src/test/java/examination/DataLayer/dao/TestExaminationDao.java
+++ src/test/java/examination/DataLayer/dao/TestExaminationDao.java
@@ -9,6 +9,11 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
 public class TestExaminationDao extends AbstractTest<Exam> {
 
     @Autowired
@@ -29,8 +34,25 @@ public class TestExaminationDao extends AbstractTest<Exam> {
         List<Question> questionList = new ArrayList<Question>();
         questionList.add(questionDAO.getRandomQuestion());
         questionList.add(questionDAO.getRandomQuestion());
+        questionList.add(questionDAO.getRandomQuestion());
         exam.setQuestions(questionList);
 
-        baseCheck(exam, examDAO);
+        boolean inserted = examDAO.insert(exam);
+        assertTrue(exam.getId() > 0);
+        assertTrue(inserted);
+
+        long id = exam.getId();
+        Exam exam1 = examDAO.selectById(id);
+        assertNotNull(exam1);
+
+        List<Question> newList = exam1.getQuestions();
+        for (int i = 0; i < questionList.size(); ++i) {
+            assertEquals(questionList.get(i).getId(), newList.get(i).getId());
+        }
+
+
+        examDAO.delete(id);
+        assertNull(examDAO.selectById(id));
+
     }
 }
