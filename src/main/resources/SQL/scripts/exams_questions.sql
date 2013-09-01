9d88b2e5dbe60eacf7770e7bc36225aa46ec5419
GodPlaysChess <pumabox@gmail.com>
2013-09-01 16:36:38 +0200

    controllers fixed

--- src/main/resources/SQL/scripts/exams_questions.sql
+++ src/main/resources/SQL/scripts/exams_questions.sql
@@ -1,16 +1,7 @@
 CREATE TABLE `questionsproject`.exams_questions (
   `exam_id`     BIGINT NOT NULL AUTO_INCREMENT,
   `question_id` BIGINT NOT NULL,
-  `index_col` BIGINT,
+  `index_col` INTEGER NOT NULL,
   PRIMARY KEY (`exam_id`, `question_id`));
 
-
-INSERT INTO `exams_questions` (exam_id, question_id) VALUES (1, 1);
-INSERT INTO `exams_questions` (exam_id, question_id) VALUES (1, 2);
-INSERT INTO `exams_questions` (exam_id, question_id) VALUES (1, 5);
-INSERT INTO `exams_questions` (exam_id, question_id) VALUES (1, 16);
-INSERT INTO `exams_questions` (exam_id, question_id) VALUES (1, 9);
-INSERT INTO `exams_questions` (exam_id, question_id) VALUES (2, 1);
-INSERT INTO `exams_questions` (exam_id, question_id) VALUES (2, 11);
-INSERT INTO `exams_questions` (exam_id, question_id) VALUES (3, 1);
 
