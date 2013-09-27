0459e4de734a9fda9bc3688251d2b6a864b0484f
GodPlaysChess <pumabox@gmail.com>
2013-09-27 21:04:14 +0200

    Examination Service Test is added

--- src/test/java/examination/DataLayer/dao/TestCourseDao.java
+++ src/test/java/examination/DataLayer/dao/TestCourseDao.java
@@ -3,6 +3,7 @@ package examination.DataLayer.dao;
 import examination.DataLayer.models.Course;
 import examination.DataLayer.models.GradingSystem;
 import examination.DataLayer.models.Question;
+import examination.DataLayer.models.enums.QuestionType;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -24,9 +25,22 @@ public class TestCourseDao  extends AbstractTest<Course> {
         course.setGradingSystem(gradingSystem);
         course.setExamQuestionsNumber(3);
         List<Question> questions = new ArrayList<Question>(1);
-        questions.add(questionDAO.selectById(1));  // Change to getRandomQuestion()
-        questions.add(questionDAO.selectById(2));  // when this method is finished
+        Question q1 = new Question();
+        q1.setText("text");
+        q1.setType(QuestionType.SIMPLE);
+        Question q2 = new Question();
+        q2.setText("text");
+        q2.setType(QuestionType.SIMPLE);
+        questionDAO.insert(q1);
+        questionDAO.insert(q2);
+        questions.add(questionDAO.selectById(q1.getId()));
+        questions.add(questionDAO.selectById(q2.getId()));
+        course.setQuestions(questions);
 
         baseCheck(course, courseDAO);
+
+        questionDAO.delete(q1.getId());
+        questionDAO.delete(q2.getId());
+
     }
 }
---
+++ src/test/java/examination/DataLayer/dao/TestExaminationService.java
@@ -0,0 +1,82 @@
+package examination.DataLayer.dao;
+
+import examination.DataLayer.models.Course;
+import examination.DataLayer.models.GradingSystem;
+import examination.DataLayer.models.Question;
+import examination.DataLayer.models.enums.QuestionType;
+import examination.QuestionService.ExaminationService;
+import examination.QuestionService.models.QuestionInfo;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static junit.framework.Assert.*;
+import static junit.framework.Assert.assertEquals;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath*:/spring/applicationContext.xml"})
+public class TestExaminationService {
+
+    @Autowired
+    private ExaminationService examinationService;
+    @Autowired
+    private CourseDAO courseDAO;
+
+    @Autowired
+    private QuestionDAO questionDAO;
+    @Autowired
+    private ExamDAO examDAO;
+
+    @Test
+    public void test() {
+        Course course = new Course();
+        GradingSystem gradingSystem = new GradingSystem();
+        course.setGradingSystem(gradingSystem);
+        course.setExamQuestionsNumber(2);
+        List<Question> questions = new ArrayList<Question>(1);
+        Question q1 = new Question();
+        q1.setText("text");
+        q1.setType(QuestionType.SIMPLE);
+        Question q2 = new Question();
+        q2.setText("text");
+        q2.setType(QuestionType.SIMPLE);
+        questionDAO.insert(q1);
+        questionDAO.insert(q2);
+        questions.add(q1);
+        questions.add(q2);
+        course.setQuestions(questions);
+        courseDAO.insert(course);
+        long courseId = course.getId();
+
+        QuestionInfo questionInfo1 = examinationService.start(2, courseId);
+        assertNotNull(questionInfo1);
+
+        long examId = questionInfo1.getExam().getId();
+        QuestionInfo questionInfo2 = examinationService.next(examId);
+        assertNotNull(questionInfo2);
+        assertNotSame(questionInfo2.getQuestion().getId(), questionInfo1.getQuestion().getId());
+        assertEquals(questionInfo1.getExam().getId(), questionInfo2.getExam().getId());
+
+        QuestionInfo questionInfo3 = examinationService.current(examId);
+        assertNotNull(questionInfo3);
+        assertEquals(questionInfo3.getQuestion().getId(), questionInfo2.getQuestion().getId());
+        assertEquals(questionInfo3.getExam().getId(), questionInfo2.getExam().getId());
+
+        examDAO.delete(examId);
+        assertNull(examDAO.selectById(examId));
+
+        courseDAO.delete(courseId);
+        assertNull(courseDAO.selectById(courseId));
+
+        questionDAO.delete(q1.getId());
+        assertNull(questionDAO.selectById(q1.getId()));
+        questionDAO.delete(q2.getId());
+        assertNull(questionDAO.selectById(q2.getId()));
+    }
+
+}
